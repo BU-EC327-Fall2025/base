76f8c4ee6f360a11580b62fb389b34e2b10a93a3
Ed Solovey <ed@digits.com>
2025-07-14 00:00:14 -0400

    ta

--- lab2/lab2_problem1_solution.cpp
+++ lab2/lab2_problem1_solution.cpp
@@ -4,7 +4,7 @@ std::string diagonalStars(int n) {
     std::string result = "";
 
     if (n < 1) {
-        return "Not Supporte";
+        return "Not Supporta";
     }
 
     for (int row = 0; row < n; row++) {
